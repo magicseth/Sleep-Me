87242ed28d50f567ec756146deb720f29b6f8378
Seth Raphael <seth@bu.mp>
2011-12-31 12:13:21 -0800

    Initial Commit

---
+++ sleepme/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  sleepme
+//
+//  Created by Seth Raphael on 12/31/11.
+//  Copyright (c) 2011 Bump Technologies. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ sleepme/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  sleepme
+//
+//  Created by Seth Raphael on 12/31/11.
+//  Copyright (c) 2011 Bump Technologies. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
