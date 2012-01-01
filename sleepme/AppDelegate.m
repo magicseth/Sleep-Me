e98072a0287a7287aa4d2aae8c0ac8a7ce4cb6f1
Seth Raphael <seth@bu.mp>
2012-01-01 00:16:39 -0800

    DONE!

--- sleepme/AppDelegate.m
+++ sleepme/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <IOKit/ps/IOPowerSources.h>
+#import <notify.h>
 
 @implementation AppDelegate
 
@@ -17,9 +19,26 @@
     [super dealloc];
 }
 
+- (void) sleep;
+{
+    system("pmset sleepnow");
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+    int outtoken;
+    int minMinutes = 10;
+    notify_register_dispatch(kIOPSTimeRemainingNotificationKey, &outtoken, dispatch_get_main_queue(), ^(int token) {
+        //
+        CFTimeInterval remaining = IOPSGetTimeRemainingEstimate();
+        IOPSLowBatteryWarningLevel  level = IOPSGetBatteryWarningLevel();
+        NSLog(@"level is %d with remaining time %f", level, remaining);
+        if ((remaining > 0 && remaining < 60* minMinutes ) || level == kIOPSLowBatteryWarningFinal) {
+            [self sleep];
+        }
+    });
 }
 
 @end
