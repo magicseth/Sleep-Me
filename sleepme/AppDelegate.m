42c15f71beafafa8df3be771f814d5469d26a8e2
Seth Raphael <seth@bu.mp>
2012-01-03 13:51:32 -0800

    add as login item

--- sleepme/AppDelegate.m
+++ sleepme/AppDelegate.m
@@ -22,6 +22,66 @@
     [super dealloc];
 }
 
+
+-(void) addAppAsLoginItem{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+    
+	// Create a reference to the shared file list.
+    // We are adding it to the current user only.
+    // If we want to add it all users, use
+    // kLSSharedFileListGlobalLoginItems instead of
+    //kLSSharedFileListSessionLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+	if (loginItems) {
+		//Insert an item to the list.
+		LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,
+                                                                     kLSSharedFileListItemLast, NULL, NULL,
+                                                                     url, NULL, NULL);
+		if (item){
+			CFRelease(item);
+        }
+	}	
+    
+	CFRelease(loginItems);
+}
+
+-(void) deleteAppFromLoginItem{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+    
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+    
+	if (loginItems) {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray  *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		int i = 0;
+		for(i = 0; i< [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray
+                                                                        objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+				NSString * urlPath = [(NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					LSSharedFileListItemRemove(loginItems,itemRef);
+				}
+			}
+		}
+		[loginItemsArray release];
+	}
+}
+
 - (void) sleep;
 {
     
@@ -57,7 +117,7 @@
         //
         CFTimeInterval remaining = IOPSGetTimeRemainingEstimate();
         IOPSLowBatteryWarningLevel  level = IOPSGetBatteryWarningLevel();
-        NSLog(@"level is %d with remaining time %f", level, remaining);
+//        NSLog(@"level is %d with remaining time %f", level, remaining);
         if (level == kIOPSLowBatteryWarningNone) {
             mode = FINE;
         }
@@ -73,6 +133,7 @@
             [self sleep];
         }
     });
+    [self addAppAsLoginItem];
 }
 
 //
